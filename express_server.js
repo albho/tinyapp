72fe53befa930288216e12499bc5b4cd56e0af24
Albert Ho <albertcyi.ho@gmail.com>
2022-01-24 16:34:52 -0800

    Add delete button and functionality

--- express_server.js
+++ express_server.js
@@ -59,6 +59,14 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log("deleting!");
+
+  delete urlDatabase[req.params.shortURL];
+
+  res.redirect("/urls");
+});
+
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
 // });
