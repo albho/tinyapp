6a677f785c73f775ec77e642fee44e1914579af4
Albert Ho <albertcyi.ho@gmail.com>
2022-01-25 12:02:19 -0800

    Add basic registration template and route handler

--- express_server.js
+++ express_server.js
@@ -88,6 +88,12 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+// display registration form
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render("register", templateVars);
+});
+
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
