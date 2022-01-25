1f8206bd6fbf04646df4aaa11c42fa5f02abdeac
Albert Ho <albertcyi.ho@gmail.com>
2022-01-24 16:41:44 -0800

    Add comments for each route handler and removed unnecessary routes

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+// simulate a database
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -27,25 +28,25 @@ function generateRandomString() {
   return randomString;
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+// home page displaying all shortURLS with corresponding longURLS
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// form for creating a new shortURL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// handle form submission for creating a new shortURL
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
+// show the newly created shortURL-longURL pair after creating a new shortURL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -54,18 +55,21 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// redirect to longURL after clicking corresponding shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// delete a particular shortURL-longURL pair
 app.post("/urls/:shortURL/delete", (req, res) => {
-  console.log("deleting!");
-
   delete urlDatabase[req.params.shortURL];
-
   res.redirect("/urls");
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
