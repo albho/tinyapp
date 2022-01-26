85ea1e285a81c2914b500d9013e1dd98d48ce997
Albert Ho <albertcyi.ho@gmail.com>
2022-01-26 14:20:11 -0800

    Implement encrypted cookies with cookie-session

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 
@@ -11,6 +11,7 @@ const { findUserId } = require("./helpers/findUserId");
 const { generateId } = require("./helpers/generateId");
 const { shortURLbelongsToUser } = require("./helpers/shortURLbelongsToUser");
 const { urlsForUser } = require("./helpers/urlsForUser");
+const SECRET_KEY = "a_terrible_secret";
 const {
   ERROR_400,
   ERROR_401,
@@ -18,13 +19,19 @@ const {
   ERROR_404,
 } = require("./helpers/errorMessages");
 
-app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "user_id",
+    keys: [SECRET_KEY],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 // ROUTE HANDLERS
 app.get("/", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
 
   if (currentUser) {
     return res.redirect("/urls");
@@ -35,8 +42,8 @@ app.get("/", (req, res) => {
 
 // render home page - list all of a user's URLs
 app.get("/urls", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
-  const currentUserUrls = urlsForUser(req.cookies["user_id"]);
+  const currentUser = userDatabase[req.session.user_id];
+  const currentUserUrls = urlsForUser(req.session.user_id);
   const templateVars = { user: currentUser, urls: currentUserUrls };
 
   if (!currentUser) {
@@ -49,7 +56,7 @@ app.get("/urls", (req, res) => {
 
 // render page for creating a new shortURL
 app.get("/urls/new", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
 
   if (!currentUser) {
     return res.redirect("/login");
@@ -61,7 +68,7 @@ app.get("/urls/new", (req, res) => {
 
 // render the newly created shortURL
 app.get("/urls/:shortURL", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
   const templateVars = { user: currentUser };
 
   if (!currentUser) {
@@ -77,7 +84,7 @@ app.get("/urls/:shortURL", (req, res) => {
   }
 
   const shortURLauthorId = urlDatabase[shortURL].userId;
-  if (shortURLauthorId !== req.cookies["user_id"]) {
+  if (shortURLauthorId !== req.session.user_id) {
     templateVars.message = ERROR_403;
     return res.status(403).render("error_page", templateVars);
   }
@@ -89,7 +96,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 // redirect to longURL after clicking on a shortURL
 app.get("/u/:shortURL", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
   const templateVars = { user: currentUser };
 
   if (!currentUser) {
@@ -109,7 +116,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 // login page
 app.get("/login", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
 
   if (currentUser) {
     return res.redirect("/urls");
@@ -121,7 +128,7 @@ app.get("/login", (req, res) => {
 
 // registration page
 app.get("/register", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
 
   if (currentUser) {
     return res.redirect("/urls");
@@ -147,7 +154,8 @@ app.post("/login", (req, res) => {
     return res.status(400).render("error_page", templateVars);
   }
 
-  res.cookie("user_id", userId);
+  // res.cookie("user_id", userId);
+  req.session.user_id = userId;
   res.redirect("/urls");
 });
 
@@ -172,16 +180,17 @@ app.post("/register", (req, res) => {
     return res.status(400).render("error_page", templateVars);
   }
 
-  const id = generateId();
+  const userId = generateId();
   const hashedPassword = bcrypt.hashSync(password, 10);
-  userDatabase[id] = { id, email, password: hashedPassword };
-  res.cookie("user_id", id);
+  userDatabase[userId] = { email, id: userId, password: hashedPassword };
+  // res.cookie("user_id", id);
+  req.session.user_id = userId;
   res.redirect("/urls");
 });
 
 // create a new shortURL
 app.post("/urls", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
   const templateVars = { user: null };
 
   if (!currentUser) {
@@ -191,13 +200,13 @@ app.post("/urls", (req, res) => {
 
   const shortURL = generateId();
   const { longURL } = req.body;
-  urlDatabase[shortURL] = { longURL, userId: req.cookies["user_id"] };
+  urlDatabase[shortURL] = { longURL, userId: req.session.user_id };
   res.redirect(`/urls/${shortURL}`);
 });
 
 // update a shortURL's longURL
 app.post("/urls/:shortURL", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
   const templateVars = { user: currentUser };
 
   if (!currentUser) {
@@ -206,7 +215,7 @@ app.post("/urls/:shortURL", (req, res) => {
   }
 
   const { shortURL } = req.params;
-  if (!shortURLbelongsToUser(shortURL, req.cookies["user_id"])) {
+  if (!shortURLbelongsToUser(shortURL, req.session.user_id)) {
     templateVars.message = ERROR_403;
     return res.status(403).render("error_page", templateVars);
   }
@@ -218,7 +227,7 @@ app.post("/urls/:shortURL", (req, res) => {
 
 // delete a particular shortURL
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const currentUser = userDatabase[req.cookies["user_id"]];
+  const currentUser = userDatabase[req.session.user_id];
   const templateVars = { user: currentUser };
 
   if (!currentUser) {
@@ -227,7 +236,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 
   const { shortURL } = req.params;
-  if (!shortURLbelongsToUser(shortURL, req.cookies["user_id"])) {
+  if (!shortURLbelongsToUser(shortURL, req.session.user_id)) {
     templateVars.message = ERROR_403;
     return res.status(403).render("error_page", templateVars);
   }
